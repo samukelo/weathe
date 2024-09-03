e16786dbb34c84d5f056de1778807c7974f95e09
Samukelo Worthwhile Makofane <samukelomakofane@gmail.com>
2024-09-03 09:35:07 +0200

    searchshownocards

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -78,8 +78,7 @@ const WeatherApp = () => {
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
-  const API_KEY_GEOCODING = "a3ce3860d56143a0827c1a50e120572d"; // Replace with your OpenCage API key
-  const API_KEY_WEATHER = "YOUR_OPEN_METEO_API_KEY"; // Replace with your Open-Meteo API key
+  const API_KEY_GEOCODING = "a3ce3860d56143a0827c1a50e120572d"; 
 
   const API_URL = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_max,temperature_2m_min,precipitation_sum&timezone=auto`;
 
@@ -151,87 +150,81 @@ const WeatherApp = () => {
   return (
     <div>
   
-  <div className="homeform">
-             
-              <form onSubmit={handleSearch}>
-                <p>
-                <input 
-                className="searchbox border-2 border-blue-600"
-                  type="text"
-                  placeholder="Enter city"
-                  value={query}
-                  onChange={(e) => setQuery(e.target.value)}
-                />
-                </p>
-                
-                <button
-                  type="submit"
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full py-4"
-                >
-                  Search
-                </button>
-              </form>
-            </div>
-        
-            {error && <p style={{ color: "red" }}>{error}</p>}
-            {dailyData.time && (
-              <div>
-                <div className="grid gap-4 grid-cols-4 py-6">
-              
-                  {getPaginatedData().map((date, index) => (
-              
-                   <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 py-4">
-                    <a href="#" key={index}>
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{date}</h5>
-                    </a>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                    <p>
-                        Max Temperature: {dailyData.temperature_2m_max[index]}°C
-                      </p>
-                      <p>
-                        Min Temperature: {dailyData.temperature_2m_min[index]}°C
-                      </p>
-                      <p>
-                        Precipitation: {dailyData.precipitation_sum[index]} mm
-                      </p>
-                      
-                    </p>
-                    <a  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    <button onClick={() => handleDetailClick(index)}>
-                        View Details
-                      </button>
-                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                        </svg>
-                    </a>
-                </div> 
+  <div>
+    <div className="homeform">
+      <form onSubmit={handleSearch}>
+        <p>
+          <input
+            className="searchbox border-2 border-blue-600"
+            type="text"
+            placeholder="Enter city"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </p>
 
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        >
+          Search
+        </button>
+      </form>
+    </div>
 
+    {error && <p style={{ color: "red" }}>{error}</p>}
 
-                  
-                    
-                  ))}
-            </div> 
-              </div>
-            )}
-            <div className="pagination space-x-4 py-4">
-              <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                onClick={() => setCurrentPage(currentPage - 1)}
-                disabled={currentPage === 1}
+    {dailyData.time && (
+      <div>
+        <div className="grid gap-4 grid-cols-4 py-6">
+          {getPaginatedData().map((date, index) => (
+            <div
+              key={index}
+              className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 py-4"
+            >
+              <a href="#">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  {date}
+                </h5>
+              </a>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                <p>Max Temperature: {dailyData.temperature_2m_max[index]}°C</p>
+                <p>Min Temperature: {dailyData.temperature_2m_min[index]}°C</p>
+                <p>Precipitation: {dailyData.precipitation_sum[index]} mm</p>
+              </p>
+              <button
+                onClick={() => handleDetailClick(index)}
+                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
-                Previous
-              </button>
-              <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 pr-4"
-                onClick={() => setCurrentPage(currentPage + 1)}
-                disabled={
-                  currentPage * itemsPerPage >=
-                  (dailyData.time ? dailyData.time.length : 0)
-                }
-              >
-                Next
+                View Details
               </button>
             </div>
-          
+          ))}
+        </div>
+      </div>
+    )}
+
+    <div className="pagination space-x-4 py-4">
+      <button
+        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        onClick={() => setCurrentPage(currentPage - 1)}
+        disabled={currentPage === 1}
+      >
+        Previous
+      </button>
+      <button
+        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        onClick={() => setCurrentPage(currentPage + 1)}
+        disabled={
+          currentPage * itemsPerPage >= (dailyData.time ? dailyData.time.length : 0)
+        }
+      >
+        Next
+      </button>
+    </div>
+
+    {/* Show City Cards only when no search query */}
+    {!query && (
       <div className="grid gap-4 grid-cols-4">
         {cities.map((cityInfo) => (
           <CityCardWithWeather
@@ -240,23 +233,13 @@ const WeatherApp = () => {
             latitude={cityInfo.latitude}
             longitude={cityInfo.longitude}
             image={cityInfo.image}
+            onClick={() => handleCityClick(cityInfo.latitude, cityInfo.longitude, cityInfo.city)}
           />
         ))}
-        {/* <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ">
-    <a href="#">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-    </a>
-    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-        Read more
-        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-        </svg>
-    </a>
-</div> */}
       </div>
-      
-    </div>
+    )}
+  </div>
+  </div>
   );
 };
 
